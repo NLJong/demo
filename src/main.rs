ba671ccb24248d7a70dc9c978dd596ab1eac8c8c
Luka Leer <luka.leer@gmail.com>
2024-07-30 21:25:36 +0200

    🐛 Fix dependencies needing to be installed
    Signed-off-by: Luka Leer <luka.leer@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use clap::{Parser, Subcommand};
-use mdbook::MDBook;
-use std::process::Command as ProcessCommand;
+use codespan_reporting::term::termcolor::ColorChoice;
+use mdbook::{errors::Error, renderer::RenderContext, MDBook, Renderer};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
-struct Args {
+struct Arguments {
     #[command(subcommand)]
     command: Command,
 }
@@ -13,30 +13,48 @@ struct Args {
 enum Command {
     #[command(about = "Build the explanation as an HTML book")]
     Build,
-    #[command(about = "Serve the explanation as an HTML book")]
-    Serve,
+}
+
+struct LinkCheckRenderer;
+
+impl LinkCheckRenderer {
+    fn new() -> Self {
+        LinkCheckRenderer
+    }
+}
+
+impl Renderer for LinkCheckRenderer {
+    fn name(&self) -> &str {
+        "linkcheck"
+    }
+
+    fn render(&self, ctx: &RenderContext) -> Result<(), Error> {
+        let mut ctx = ctx.clone();
+        ctx.config
+            .set("output.linkcheck.follow-web-links", true)
+            .expect("Unable to set linkcheck config");
+
+        let cache_file = ctx.destination.join("linkcheck_cache.json");
+        let cache_file = Some(cache_file.as_path());
+
+        mdbook_linkcheck::run(cache_file, ColorChoice::Auto, &ctx, None)
+    }
 }
 
 fn main() {
     colog::init();
 
-    let arguments = Args::parse();
-
+    let arguments = Arguments::parse();
     let book_root = env!("CARGO_MANIFEST_DIR");
-    let book = MDBook::load(book_root).expect("Unable to load book");
+
+    let mut book = MDBook::load(book_root).expect("Unable to load book");
+    let book = book
+        .with_preprocessor(mdbook_alerts::Preprocessor)
+        .with_renderer(LinkCheckRenderer::new());
 
     match &arguments.command {
         Command::Build => {
             book.build().expect("Unable to build book");
-        }
-        Command::Serve => {
-            ProcessCommand::new("mdbook")
-                .arg("serve")
-                .arg(book_root)
-                .spawn()
-                .expect("Unable to serve book")
-                .wait()
-                .expect("Unable to serve book");
         }
     };
 }
